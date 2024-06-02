ef51a7f5857f88435f47bea6b8b9c881c443ace5
Kibieptr <138297239+Lynnxha@users.noreply.github.com>
2024-06-02 13:47:43 +0700

    usable style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { requestToGroq } from "./utils/groq";
 import { useState } from "react";
-import { Light as SyntaxHighLight } from "react-syntax-highlighter";
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import "./App.css";
 
@@ -9,6 +9,7 @@ function App() {
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () => {
+    const content = document.getElementById("content");
     setLoading(true);
     setTimeout(async () => {
       const ai = await requestToGroq(content.value);
@@ -17,36 +18,50 @@ function App() {
     }, 2000);
   };
 
+  const customStyle = {
+    ...oneDark,
+    'code[class*="language-"]': {
+      ...oneDark['code[class*="language-"]'],
+      fontFamily: "sans-serif, italic",
+    },
+  };
+
   return (
     <main className="flex flex-col min-h-[80vh] justify-center items-center max-w-xl w-full mx-auto">
       <h1 className="text-4xl text-indigo-500">Piyyuu Chat AI</h1>
       <form className="flex flex-col gap-4 py-4 w-full">
-        <input
-          placeholder="Type your message here"
-          className="py-6 px-4 text-md rounded-md focus:outline-none"
-          id="content"
-          type="text"
-        />
-        <button
-          onClick={handleSubmit}
-          type="button"
-          className={`py-2 px-4 font-bold text-white rounded-md flex justify-center items-center ${
-            loading ? "bg-gray-400" : "bg-indigo-500"
-          }`}
-          disabled={loading}
-        >
-          {loading ? <div className="spinner"></div> : "Send"}
-        </button>
+        <div className="flex gap-2">
+          <input
+            placeholder="Type your message here"
+            className="py-4 px-4 text-md rounded-md flex-grow focus:outline-none"
+            id="content"
+            type="text"
+          />
+          <button
+            onClick={handleSubmit}
+            type="button"
+            className={`py-4 px-5 font-bold text-white rounded-md flex justify-center items-center ${
+              loading ? "bg-gray-400" : "bg-indigo-500"
+            }`}
+            disabled={loading}
+          >
+            {loading ? (
+              <div className="spinner"></div>
+            ) : (
+              <i className="fas fa-paper-plane"></i>
+            )}
+          </button>
+        </div>
       </form>
       <div className="max-w-xl w-full mx-auto">
         {data ? (
-          <SyntaxHighLight
-            language="swift"
-            style={oneDark}
+          <SyntaxHighlighter
+            language="bash"
+            style={customStyle}
             wrapLongLines={true}
           >
             {data}
-          </SyntaxHighLight>
+          </SyntaxHighlighter>
         ) : null}
       </div>
     </main>
